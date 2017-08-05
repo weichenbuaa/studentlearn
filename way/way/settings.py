edc0d36d7adb44ea1518d313524ebd7f58fd0e0e
Wei CHEN <wei.chen@labri.fr>
2017-08-05 15:49:38 +0200

    mysql

--- way/way/settings.py
+++ way/way/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'waybackground'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,16 @@ WSGI_APPLICATION = 'way.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'way',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
