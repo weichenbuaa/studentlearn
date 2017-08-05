9e2e440bf3e7a55157345ce862a8c05839a46fa3
Wei CHEN <wei.chen@labri.fr>
2017-08-05 15:52:58 +0200

    sql-togenerate-models

--- way/waybackground/models.py
+++ way/waybackground/models.py
@@ -1,6 +1,295 @@
-# -*- coding: utf-8 -*-
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class CustomProcess(models.Model):
+    step = models.CharField(max_length=100, blank=True, null=True)
+    rank = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'custom_process'
+
+
+class CustomStep(models.Model):
+    before = models.IntegerField(blank=True, null=True)
+    step_id = models.IntegerField(blank=True, null=True)
+    next = models.IntegerField(blank=True, null=True)
+    content = models.CharField(max_length=260, blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'custom_step'
+
+
+class Customer(models.Model):
+    mobile = models.CharField(max_length=11, blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    wechat = models.CharField(max_length=100, blank=True, null=True)
+    qq = models.CharField(max_length=18, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    peoples = models.IntegerField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    partner = models.CharField(max_length=50, blank=True, null=True)
+    context = models.CharField(max_length=500, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'customer'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Menu(models.Model):
+    menu = models.CharField(max_length=11, blank=True, null=True)
+    url = models.CharField(max_length=200, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    upate_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'menu'
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    type = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'question'
+
+
+class QuestionOption(models.Model):
+    question_id = models.CharField(max_length=36, blank=True, null=True)
+    content = models.CharField(max_length=255, blank=True, null=True)
+    checked = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'question_option'
+
+
+class Result(models.Model):
+    mobile = models.CharField(max_length=11, blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    wechat = models.CharField(max_length=100, blank=True, null=True)
+    qq = models.CharField(max_length=18, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    context = models.CharField(max_length=500, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'result'
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=50, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    upate_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'role'
+
+
+class RoleMenu(models.Model):
+    role = models.ForeignKey(Role, models.DO_NOTHING, blank=True, null=True)
+    menu = models.ForeignKey(Menu, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'role_menu'
+
+
+class Staff(models.Model):
+    account = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=32, blank=True, null=True)
+    role_id = models.IntegerField(blank=True, null=True)
+    last_sign_time = models.DateTimeField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'staff'
+
+
+class StoryType(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    upate_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'story_type'
+
+
+class System(models.Model):
+    site_key = models.CharField(max_length=50, blank=True, null=True)
+    site_value = models.CharField(max_length=100, blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'system'
+
+
+class Tactic(models.Model):
+    before = models.IntegerField(blank=True, null=True)
+    question_id = models.IntegerField(blank=True, null=True)
+    next = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tactic'
+
+
+class TravelStory(models.Model):
+    type = models.IntegerField(blank=True, null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    staff_id = models.IntegerField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    upate_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'travel_story'
