e7f05ce93bc5129bf89591772858f247267b48bf
Wei CHEN <wei.chen@labri.fr>
2017-08-22 18:00:10 +0200

    customer add result

--- way/waybackground/views.py
+++ way/waybackground/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 import json
+import string
 
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -49,3 +51,51 @@ def process_show(request):
     # 从数据库中获取已经存在的流程数据，将数据在传递到前端界面
     proList = CustomProcess.objects.filter(status=0).order_by('rank')
     return render(request, 'process_show.html', {'proList': proList})
+
+# 旅游客户信息管理
+def customer_add(request):
+    return render(request, 'customer_add.html')
+
+# 旅游客户信息管理
+def customer_add_result(request):
+    name = request.POST['name']
+    mobile = request.POST['mobile']
+    email = request.POST['email']
+    wechat = request.POST['wechat']
+    qq = request.POST['qq']
+    destination = request.POST['destination']
+    start_date = datetime.datetime.strptime(request.POST['start_date'], '%m/%d/%Y').date()
+    end_date = datetime.datetime.strptime(request.POST['end_date'], '%m/%d/%Y').date()
+    days = request.POST['days']
+    partner = request.POST['partner']
+    peoples = request.POST['peoples']
+    budget = request.POST['budget']
+    air_tickets = string.atoi(request.POST['air_tickets'])
+    visa = string.atoi(request.POST['visa'])
+    context = request.POST['context']
+    nowTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    print name
+    print mobile
+    print email
+    print wechat
+    print qq
+    print destination
+    print start_date
+    print end_date
+    print days
+    print partner
+    print peoples
+    print budget
+    print air_tickets
+    print visa
+    print context
+
+    customer = Customer(name=name, mobile= mobile, email= email, wechat= wechat, qq= qq,
+                        destination = destination, start_date = start_date, end_date= end_date,
+                        days = days, partner = partner,peoples = peoples, budget = budget,
+                        air_tickets = air_tickets, visa = visa, context = context,
+                        create_time = nowTime, status= 0)
+    customer.save()
+
+    return render(request, 'index.html')
