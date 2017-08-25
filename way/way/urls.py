5e384e2cd4bf5337b10d3c2b192bba28fe21d181
Wei CHEN <wei.chen@labri.fr>
2017-08-25 12:02:04 +0200

    customer add 和edit

--- way/way/urls.py
+++ way/way/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     url(r'^process_show/$', process_show,name='process_show'),
     url(r'^customer_add/$', customer_add,name='customer_add'),
     url(r'^customer_add_result/$', customer_add_result,name='customer_add_result'),
+    url(r'^customer_show/$', customer_show,name='customer_show'),
+    url(r'^customer_edit/$', customer_edit,name='customer_edit'),
 ]
